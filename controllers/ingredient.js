7826f248d698b4a450b02471a92bae1910d88a20
veenaprakash123 <veenaprakash2018@gmail.com>
2022-03-20 16:28:09 -0400

    Routes for adding and editing an ingredient now included

--- controllers/ingredient.js
+++ controllers/ingredient.js
@@ -13,6 +13,41 @@ router.get('/', (req, res) => {
 })
 
 
+router.get('/details/:id', (req,res)=> {
+    Ingredient.findById(req.params.id, (error, ingredient) => {
+        if (error){
+            res.status(400).json({error:error.message})
+        }
+        console.log(ingredient)
+        res.status(200).json(ingredient)
+    })
+})
+
+// Add Ingredient
+
+router.post('/', (req, res, next) => {
+    Ingredient.create(req.body, (error, newIngredient) => {
+        console.log(req.body)
+        if(error){
+            res.status(400).json({error: error.message})
+        }
+        res.status(200).json(newIngredient)
+    })
+})
+
+
+// Edit Ingredient 
+
+router.put('/edit/:id', (req,res) => {
+    Ingredient.findByIdAndUpdate(req.params.id, req.body, {new:true}, (error, updatedIngredient) => {
+        if(error){
+            res.status(400).json({error: error.message})
+            console.log(error)
+        }
+        console.log(updatedIngredient)
+        res.status(200).json(updatedIngredient)
+    })
+})
 
 
 
