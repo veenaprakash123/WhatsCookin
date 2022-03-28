cbfc585b16ce3ac7fbc161df8d49a8e22d9a3f39
veenaprakash123 <veenaprakash2018@gmail.com>
2022-03-27 23:08:54 -0400

    Added some styling to Kitchen component

--- db/ingredientSeeds.js
+++ db/ingredientSeeds.js
@@ -7,8 +7,8 @@ Ingredient.deleteMany({})
         {
             "name": 'pear',
             "amount": '3',
-            "location": 'fruit basket',
-            "image": 'pic of apple',
+            "location": 'fridge',
+            "image": 'pic of pear',
         },
         {
             "name": 'pasta',
@@ -22,6 +22,54 @@ Ingredient.deleteMany({})
             "location": 'fridge',
             "image": 'pic of tomato',
         },
+        {
+            "name": 'salt',
+            "amount": '1 jar',
+            "location": 'pantry',
+            "image": 'pic of salt',
+        },
+        {
+            "name": 'bread',
+            "amount": '1 box',
+            "location": 'pantry',
+            "image": 'pic of bread',
+        },
+        {
+            "name": 'cous cous',
+            "amount": '1 jar',
+            "location": 'pantry',
+            "image": 'pic of cous cous',
+        },
+        {
+            "name": 'flour',
+            "amount": '1 bag',
+            "location": 'pantry',
+            "image": 'pic of flour',
+        }, 
+        {
+            "name": 'mustard',
+            "amount": '1 bottle',
+            "location": 'fridge',
+            "image": 'pic of mustard',
+        },
+        {
+            "name": 'pickles',
+            "amount": '1 jar',
+            "location": 'fridge',
+            "image": 'pic of pickles',
+        },
+        {
+            "name": 'bell peppers',
+            "amount": '7',
+            "location": 'fridge',
+            "image": 'pic of bell peppers',
+        }, 
+        {
+            "name": 'golden kiwi',
+            "amount": '6',
+            "location": 'fridge',
+            "image": 'pic of kiwi',
+        }
 
     ])
 })
