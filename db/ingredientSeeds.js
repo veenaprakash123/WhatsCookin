f3efc485399f4c4d2a9d789ccc271f16f754be83
veenaprakash123 <veenaprakash2018@gmail.com>
2022-04-01 23:20:47 -0700

    Added styling to welcome and landing pages

--- db/ingredientSeeds.js
+++ db/ingredientSeeds.js
@@ -27,49 +27,49 @@ Ingredient.deleteMany({})
             "name": 'salt',
             "amount": '1 jar',
             "location": 'pantry',
-            "image": 'pic of salt',
+            "image": 'https://media.istockphoto.com/photos/crystals-of-shallow-salt-in-a-scoop-spoon-on-a-dark-gray-table-for-picture-id1051727580?k=20&m=1051727580&s=612x612&w=0&h=v3gWwwVtHrz6fNouAUAydniYE9SedKbORry0cE-Kzq0=',
         },
         {
             "name": 'bread',
             "amount": '1 box',
             "location": 'pantry',
-            "image": 'pic of bread',
+            "image": 'https://healthynibblesandbits.com/wp-content/uploads/2019/12/Milk-Bread-FF2.jpg',
         },
         {
             "name": 'cous cous',
             "amount": '1 jar',
             "location": 'pantry',
-            "image": 'pic of cous cous',
+            "image": 'https://images.albertsons-media.com/is/image/ABS/960438259?$ecom-pdp-desktop$&defaultImage=Not_Available&defaultImage=Not_Available',
         },
         {
             "name": 'flour',
             "amount": '1 bag',
             "location": 'pantry',
-            "image": 'pic of flour',
+            "image": 'https://www.meijer.com/content/dam/meijer/product/0001/60/0010/61/0001600010610_2_A1C1_1200.png',
         }, 
         {
             "name": 'mustard',
             "amount": '1 bottle',
             "location": 'fridge',
-            "image": 'pic of mustard',
+            "image": 'https://m.media-amazon.com/images/I/71iNDByzp8L._SL1500_.jpg',
         },
         {
             "name": 'pickles',
             "amount": '1 jar',
             "location": 'fridge',
-            "image": 'pic of pickles',
+            "image": 'https://media.istockphoto.com/photos/pickled-gherkins-picture-id171292196?k=20&m=171292196&s=612x612&w=0&h=TmvFrmpggqn2mSY8jyGxlyLiEMxQgGp8kLFVaTPwb0M=',
         },
         {
             "name": 'bell peppers',
             "amount": '7',
             "location": 'fridge',
-            "image": 'pic of bell peppers',
+            "image": 'https://images.albertsons-media.com/is/image/ABS/184710682?$ecom-pdp-desktop$&defaultImage=Not_Available&defaultImage=Not_Available',
         }, 
         {
             "name": 'golden kiwi',
             "amount": '6',
             "location": 'fridge',
-            "image": 'pic of kiwi',
+            "image": 'https://www.theblackpeppercorn.com/wp-content/uploads/2013/03/golden-kiwi-4x3a.jpg',
         }
 
     ])
